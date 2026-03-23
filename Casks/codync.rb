dfbb9c044a2dfc5a33b4600492da0a54af05ed2f
libokai <kevin2005ha@gmail.com>
2026-03-24 01:58:04 +0800

    Switch to DMG distribution

--- Casks/codync.rb
+++ Casks/codync.rb
@@ -1,8 +1,8 @@
 cask "codync" do
   version "1.0.0"
-  sha256 "957a9732a370b3ee8c9268a744a0bb40fa8a28bbb24677fa6fe7c1ac542847b9"
+  sha256 "35e2e15d8bcc08daf763c9374b191a38888882c04af12a425e4fbbd5878154f3"
 
-  url "https://github.com/leepokai/CodePulse/releases/download/v#{version}/Codync-macOS.zip"
+  url "https://github.com/leepokai/CodePulse/releases/download/v#{version}/Codync-macOS.dmg"
   name "Codync"
   desc "Real-time Claude Code session monitor for macOS menu bar"
   homepage "https://codync.dev"
